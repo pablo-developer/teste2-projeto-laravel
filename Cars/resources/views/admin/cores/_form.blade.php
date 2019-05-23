7e8c195c74ef93a95f5f70148fc1c697b0e65d05
pablo-developer <50967843+pablo-developer@users.noreply.github.com>
2019-05-23 18:50:17 -0300

    Lixo

--- Cars/resources/views/admin/cores/_form.blade.php
+++
@@ -1,8 +0,0 @@
-<style>
-    input.form-control{
-        text-transform: uppercase;        
-    }
-</style>
-<div class="form-group">        
-    <input class="form-control" type="text" name="corNome" class="validade" placeholder="Nome" value="{{isset($cores->corNome) ? $cores->corNome:''}}">                
-</div>
--- Cars/resources/views/admin/login/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div align="center" class="container">               
-        @include('admin.login._form')    
-</div>
-
-@endsection
